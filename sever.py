457c85f16aeb7778492de2c5abb5f4ac7f16b113
Huỳnh Mai Nhật Minh <40135676+Mark-Jethro@users.noreply.github.com>
2022-12-16 13:41:21 +0700

    Update sever.py

--- sever.py
+++ sever.py
@@ -1,11 +1,45 @@
-import os ,hashlib as D ,requests as E ,sys #line:1
-from rich .console import Console as A #line:2
-B =A ()#line:3
-def C ():#line:4
-	OOOO0000O0OOOOO0O ='code';OO0000OO00OO0O000 =os .popen ('hostname').read ();OO0000OO00OO0O000 =str (OO0000OO00OO0O000 )[:-1 ]#line:5
-	if OO0000OO00OO0O000 =='localhost':OO0000OO00OO0O000 =os .popen ('whoami').read ();OO0000OO00OO0O000 =OO0000OO00OO0O000 [:-1 ]#line:6
-	OO0000OO00OO0O000 =D .md5 (OO0000OO00OO0O000 .encode ('utf-8')).hexdigest ();OO0000OO00OO0O000 =OO0000OO00OO0O000 .upper ();O000OOOOOOOO00OO0 ='196629';OO00O0000O0000000 =E .get (f"https://tool.yenpro.net/check/key.php?code={OO0000OO00OO0O000}&order_id={O000OOOOOOOO00OO0}").json ()#line:7
-	if OO00O0000O0000000 [OOOO0000O0OOOOO0O ]==0 :B .print ('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Hệ Thống Thông Báo. Thiết Bị Của Bạn Chưa Được Kích Hoạt');B .print ('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Vui Lòng Truy Cập Vào https://tool.yenpro.net/ Để Được Kích Hoạt');B .print (f"\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]CODE Thiết Bị: {OO0000OO00OO0O000} (Vui Lòng Không Chia Sẻ CODE Cho Bất Cứ Ai)");sys .exit ()#line:8
-	elif OO00O0000O0000000 [OOOO0000O0OOOOO0O ]==1 :0 #line:9
-	else :B .print ('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Hệ Thống Thông Báo. Thiết Bị Của Bạn Đã Hết Hạng Sử Dụng');B .print ('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Vui Lòng Truy Cập Vào https://tool.yenpro.net/ Để Gia Hạn Thiết Bị');B .print (f"\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]CODE Thiết Bị: {OO0000OO00OO0O000} (Vui Lòng Không Chia Sẻ CODE Cho Bất Cứ Ai)");sys .exit ()#line:10
-if __name__ =='__main__':C ()
+# -*- coding: utf-8 -*-
+import os
+import hashlib
+import requests
+import sys
+from rich.console import Console
+
+console = Console()
+
+
+def main():
+    output = os.popen('hostname').read()
+    output = str(output)[:-1]
+    if output == 'localhost':
+        output = os.popen('whoami').read()
+        output = output[:-1]
+
+    output = hashlib.md5(output.encode('utf-8')).hexdigest()
+    output = output.upper()
+
+    order_id = '196629'
+    response = requests.get(f'https://tool.yenpro.net/check/key.php?'
+                            f'code={output}&order_id={order_id}').json()
+    if response['code'] == 0:
+        console.print('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Hệ Thống Thông Báo. '
+                      'Thiết Bị Của Bạn Chưa Được Kích Hoạt')
+        console.print('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Vui Lòng Truy Cập Vào '
+                      'https://tool.yenpro.net/ Để Được Kích Hoạt')
+        console.print(f'\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]CODE Thiết Bị: {output} '
+                      f'(Vui Lòng Không Chia Sẻ CODE Cho Bất Cứ Ai)')
+        sys.exit()
+    elif response['code'] == 1:
+        pass
+    else:
+        console.print('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Hệ Thống Thông Báo. '
+                      'Thiết Bị Của Bạn Đã Hết Hạng Sử Dụng')
+        console.print('\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]Vui Lòng Truy Cập Vào '
+                      'https://tool.yenpro.net/ Để Gia Hạn Thiết Bị')
+        console.print(f'\n[bold][bold yellow][[bold red]![bold yellow]] [bold dim cyan]CODE Thiết Bị: {output} '
+                      f'(Vui Lòng Không Chia Sẻ CODE Cho Bất Cứ Ai)')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
